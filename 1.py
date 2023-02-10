0adad43de2a593aad03001cc41e6aa5b134826ab
Loginov10 <120027474+Loginov10@users.noreply.github.com>
2023-02-10 14:00:18 +0200

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,24 @@
+from Fun2 import *
+while True:
+    #5 Банковский вклад 
+    
+
+    
+    #4 Времена года
+    aa=season(int(input("Введи время года: ")))
+    print(aa)
+
+    #3 Квадрат 
+    f=square(int(input("a: ")))
+    print(f)
+
+
+    #2 Высокосный год
+    a=is_year_leap(int(input("Введите год: ")))
+    print(a)
+    
+
+
+    #1 Простейшие арифметические операции 
+    vastus=arithmetic(int(input("a:")),int(input("b:"))),input("tehe")
+    print(vastus)
